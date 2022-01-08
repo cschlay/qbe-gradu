583eb18c346488b0097bea4aae3dde7cce8ea7be
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-01-08 19:05:25 +0200

    Add refactor Comparison so that double is supported and add test to it

---
+++ src/main/java/core/utilities/Comparison.java
@@ -0,0 +1,43 @@
+package core.utilities;
+
+
+/**
+ * Provides comparators for Object -data type that are used in store dynamic values
+ * in Neo4j database and graph entities.
+ *
+ * The implementation is minimalistic and only support "double" and "int" data types.
+ * The comparison uses "double" as a common data type so that comparison is possible between double and int.
+ */
+public class Comparison {
+    private Comparison() {}
+
+    public static boolean greaterThan(Object left, Object right) {
+        return normalize(left) > normalize(right);
+    }
+
+    public static boolean greaterThanOrEqualTo(Object left, Object right) {
+        return normalize(left) >= normalize(right);
+    }
+
+    public static boolean lessThan(Object left, Object right) {
+        return normalize(left) < normalize(right);
+    }
+
+    public static boolean lessThanOrEqualTo(Object left, Object right) {
+        return normalize(left) <= normalize(right);
+    }
+
+    /**
+     * Normalizes Object numbers so that they can be compared.
+     *
+     * @param number to normalize
+     * @return number in normalized format
+     */
+    private static double normalize(Object number) {
+        if (number instanceof Integer) {
+            return (int) number;
+        }
+
+        return (double) number;
+    }
+}
