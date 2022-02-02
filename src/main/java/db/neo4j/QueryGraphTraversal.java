549153f74ae8780885e9731c4080c03dc60630aa
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-02-02 20:15:54 +0200

    Add a simple path test to QueryGraphTraversalTest and remove buggy cache mechanism

--- src/main/java/db/neo4j/QueryGraphTraversal.java
+++ src/main/java/db/neo4j/QueryGraphTraversal.java
@@ -6,27 +6,18 @@ import core.graphs.QbeEdge;
 import core.graphs.QbeNode;
 import core.graphs.QueryGraph;
 import core.graphs.ResultGraph;
-import org.jetbrains.annotations.Nullable;
 import org.neo4j.graphdb.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class QueryGraphTraversal {
     private final Transaction tx;
-    private final HashMap<Long, QbeEdge> validEdges;
-    private final HashMap<Long, QbeNode> validNodes;
-    private final ArrayList<Long> pendingEdges;
     private final HashMap<Long, QbeNode> pendingNodes;
 
 
     public QueryGraphTraversal(Transaction transaction) {
         tx = transaction;
-        validEdges = new HashMap<>();
-        validNodes = new HashMap<>();
-
-        pendingEdges = new ArrayList<>();
         pendingNodes = new HashMap<>();
     }
 
@@ -37,10 +28,7 @@ public class QueryGraphTraversal {
             ResourceIterator<Node> nodes = tx.findNodes(Label.label(queryNode.name));
             nodes.forEachRemaining(node -> {
                 try {
-                    @Nullable QbeNode resultNode = validNodes.get(node.getId());
-                    if (resultNode == null) {
-                        resultNode = traverseNode(node, queryNode);
-                    }
+                    var resultNode = traverseNode(node, queryNode);
                     resultGraph.put(resultNode);
                 } catch (InvalidNodeException | IdConstraintException expected) {
                     // Node has invalid properties or edges, should be ignored.
@@ -60,41 +48,33 @@ public class QueryGraphTraversal {
             var edges = node.getRelationships(RelationshipType.withName(queryEdge.name));
             AtomicBoolean found = new AtomicBoolean(false);
             edges.forEach(edge -> {
-                System.out.printf("%s: %s -%s- %s%n", queryNode.name, edge.getStartNodeId(), edge.getId(), edge.getEndNodeId());
                 try {
-                    @Nullable QbeEdge resultEdge = validEdges.get(edge.getId());
-                    if (resultEdge == null) {
-                        // Find edge
-                        resultEdge = new QbeEdge(edge.getId(), queryEdge.name);
-                        resultEdge.properties.putAll(new Neo4jPropertyTraversal(queryEdge).getProperties(edge));
+                    var resultEdge = new QbeEdge(edge.getId(), queryEdge.name);
+                    resultEdge.properties.putAll(new Neo4jPropertyTraversal(queryEdge).getProperties(edge));
 
-                        if (queryEdge.tailNode != null ) {
-                            if (queryEdge.tailNode.name.equals(resultNode.name)) {
-                                resultEdge.tailNode = resultNode;
-                            } else if (pendingNodes.containsKey(edge.getStartNodeId())) {
-                                resultEdge.tailNode = pendingNodes.get(edge.getStartNodeId());
-                            } else {
-                                resultEdge.tailNode = traverseNode(edge.getStartNode(), queryEdge.tailNode);
-                            }
+                    if (queryEdge.tailNode != null ) {
+                        if (queryEdge.tailNode.name.equals(resultNode.name)) {
+                            resultEdge.tailNode = resultNode;
+                        } else if (pendingNodes.containsKey(edge.getStartNodeId())) {
+                            resultEdge.tailNode = pendingNodes.get(edge.getStartNodeId());
+                        } else {
+                            resultEdge.tailNode = traverseNode(edge.getStartNode(), queryEdge.tailNode);
                         }
-                        if (queryEdge.headNode != null) {
-                            if (queryEdge.headNode.name.equals(resultNode.name)) {
-                                resultEdge.headNode = resultNode;
-                            } else if (pendingNodes.containsKey(edge.getEndNodeId())) {
-                                resultEdge.headNode = pendingNodes.get(edge.getEndNodeId());
-                            } else {
-                                resultEdge.headNode = traverseNode(edge.getEndNode(), queryEdge.headNode);
-                            }
+                    }
+                    if (queryEdge.headNode != null) {
+                        if (queryEdge.headNode.name.equals(resultNode.name)) {
+                            resultEdge.headNode = resultNode;
+                        } else if (pendingNodes.containsKey(edge.getEndNodeId())) {
+                            resultEdge.headNode = pendingNodes.get(edge.getEndNodeId());
+                        } else {
+                            resultEdge.headNode = traverseNode(edge.getEndNode(), queryEdge.headNode);
                         }
                     }
 
                     resultNode.edges.put(resultEdge.id, resultEdge);
-                    validEdges.put(resultEdge.longId(), resultEdge);
                     found.set(true);
                 } catch (InvalidNodeException | IdConstraintException expected) {
-                    //System.out.println("ERR");
-                    //expected.printStackTrace();
-                    // Expected
+                    // Either tail or head node is invalid so the edge is discarded.
                 }
             });
 
@@ -104,7 +84,6 @@ public class QueryGraphTraversal {
             }
         }
 
-        validNodes.put(resultNode.longId(), resultNode);
         pendingNodes.remove(resultNode.longId());
         return resultNode;
     }
--- src/test/java/db/neo4j/QueryGraphTraversalTest.java
+++ src/test/java/db/neo4j/QueryGraphTraversalTest.java
@@ -1,13 +1,121 @@
 package db.neo4j;
 
 import base.QueryBaseTest;
+import core.graphs.QbeEdge;
+import core.graphs.QbeNode;
+import core.graphs.QueryGraph;
 import org.junit.jupiter.api.Test;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class QueryGraphTraversalTest extends QueryBaseTest {
+    static class Labels {
+        public static Label book = Label.label("Book");
+        public static Label course = Label.label("Course");
+        public static Label topic = Label.label("Topic");
+    }
+    static class Relations {
+        public static RelationshipType contains = RelationshipType.withName("contains");
+        public static RelationshipType uses = RelationshipType.withName("uses");
+    }
+
     @Test
     void simplePath() throws Exception {
+        // Database contains two path fragments and one full path.
+        // The result should yield the full path.
+        var fx = new Object() { Node book; Node course; Node topic; };
         run(tx -> {
+            var f1Book = tx.createNode(Labels.book);
+            var f1Course = tx.createNode(Labels.course);
+            f1Course.createRelationshipTo(f1Book, Relations.uses);
 
+            fx.course = tx.createNode(Labels.course);
+            fx.book = tx.createNode(Labels.book);
+            fx.topic = tx.createNode(Labels.topic);
+            fx.course.createRelationshipTo(fx.book, Relations.uses);
+            fx.book.createRelationshipTo(fx.topic, Relations.contains);
+
+            var f2Book = tx.createNode(Labels.book);
+            var f2Topic = tx.createNode(Labels.topic);
+            f2Book.createRelationshipTo(f2Topic, Relations.contains);
+            tx.commit();
+        });
+
+        var book = new QbeNode("Book");
+        var course = new QbeNode("Course");
+        var topic = new QbeNode("Topic");
+
+        var e1 = new QbeEdge("uses");
+        e1.tailNode = course;
+        e1.headNode = book;
+
+        var e2 = new QbeEdge("contains");
+        e2.tailNode = book;
+        e2.headNode = topic;
+
+        var queryGraph = new QueryGraph();
+        queryGraph.put(e1);
+        queryGraph.put(e2);
+
+        run(tx -> {
+            var tr = new QueryGraphTraversal(tx);
+            var resultGraph = tr.traverse(queryGraph);
+            assertEquals(3, resultGraph.order());
+            assertTrue(resultGraph.containsKey(String.valueOf(fx.course.getId())));
+            assertTrue(resultGraph.containsKey(String.valueOf(fx.book.getId())));
+            assertTrue(resultGraph.containsKey(String.valueOf(fx.topic.getId())));
+        });
+    }
+
+    @Test
+    void simpleCycle() throws Exception {
+        // Query graph has cycle
+    }
+
+    /**
+     * Db: (v1, e1, v2) and (v2, e2, v3) without (e1, v2, e2)
+     * Query: (v1, e1, v2, e2, v3)
+     * Result: None
+     */
+    @Test
+    void excludePartialPaths() throws Exception {
+        run(tx -> {
+            var v1 = tx.createNode(Label.label("v1"));
+            var v2a = tx.createNode(Label.label("v2"));
+            var v2b = tx.createNode(Label.label("v2"));
+            var v3 = tx.createNode(Label.label("v3"));
+
+            v1.createRelationshipTo(v2a, RelationshipType.withName("e1"));
+            v2b.createRelationshipTo(v3, RelationshipType.withName("e2"));
+            tx.commit();
+        });
+
+        var v1 = new QbeNode("v1");
+        var v2 = new QbeNode("v2");
+        var v3 = new QbeNode("v3");
+
+        var e1 = new QbeEdge("e1");
+        e1.tailNode = v1;
+        e1.headNode = v2;
+
+        var e2 = new QbeEdge("e2");
+        e2.tailNode = v2;
+        e2.headNode = v3;
+
+        var queryGraph = new QueryGraph();
+        queryGraph.put(v1);
+        queryGraph.put(v2);
+        queryGraph.put(v3);
+        queryGraph.put(e1);
+        queryGraph.put(e2);
+
+        run(tx -> {
+           var tr = new QueryGraphTraversal(tx);
+           var resultGraph = tr.traverse(queryGraph);
+           assertTrue(resultGraph.isEmpty(), "Graph is not empty!");
         });
     }
 }
