88f6da19dcde7dfcb1a84c4305ab24064ff910ec
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-03-13 21:21:31 +0200

    Rewrite the path aggregation tests and add more cases

---
+++ src/test/java/queries/paths/AggregateCountTest.java
@@ -0,0 +1,70 @@
+package queries.paths;
+
+import base.QueryBaseStaticTest;
+import graphs.ResultGraph;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Path - Aggregate Count")
+class AggregateCountTest extends QueryBaseStaticTest {
+    private static String idA;
+    private static String idB;
+
+    @BeforeAll
+    static void setup() throws Exception {
+        run(tx -> {
+            Node bookA = tx.createNode(Label.label("Book"));
+            Node bookB = tx.createNode(Label.label("Book"));
+            Node topicA = tx.createNode(Label.label("Topic"));
+            Node topicB = tx.createNode(Label.label("Topic"));
+
+            RelationshipType edgeName = RelationshipType.withName("contains");
+            bookA.createRelationshipTo(topicA, edgeName).setProperty("theory", false);
+            bookA.createRelationshipTo(topicB, edgeName).setProperty("theory", true);
+            bookB.createRelationshipTo(topicA, edgeName).setProperty("theory", false);
+            tx.commit();
+
+            idA = String.valueOf(bookA.getId());
+            idB = String.valueOf(bookB.getId());
+        });
+    }
+
+    @Test
+    @DisplayName("[Table] Count Edges by Node Group")
+    void countEdgesByNodeGroup() throws Exception {
+        var query = "" +
+                "| Book  | id* | contains              |\n" +
+                "|-------+-----+-----------------------|\n" +
+                "| QUERY |     | COUNT Book.Topic Book |";
+
+        ResultGraph resultGraph = execute(query);
+        assertEquals(2, resultGraph.get(idA).property("_agg-count"));
+        assertEquals(1, resultGraph.get(idB).property("_agg-count"));
+    }
+
+    @Test
+    @DisplayName("[Table] Count Edges Filtered by Properties")
+    void countFilteredEdges() throws Exception {
+        var query = "" +
+                "| Book  | id* | contains              | theory* |\n" +
+                "|-------+-----+-----------------------+---------|\n" +
+                "| QUERY |     | COUNT Book.Topic Book | true    |";
+        assertEquals(1, execute(query).get(idA).property("_agg-count"));
+    }
+
+    @Test
+    @DisplayName("[Table] Count Nodes Filtered by Edges")
+    void countNodesFilteredByEdges() throws Exception {
+        var query = "" +
+                "| Book  | contains         | theory* |\n" +
+                "|-------+------------------+---------|\n" +
+                "| COUNT | QUERY Book.Topic | false    |";
+        assertEquals(2, execute(query).get("Book").property("_agg-count") );
+    }
+}
---
+++ src/test/java/queries/paths/AggregateSumTest.java
@@ -0,0 +1,97 @@
+package queries.paths;
+
+import base.QueryBaseStaticTest;
+import graphs.ResultGraph;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@DisplayName("Path - Aggregate Sum")
+class AggregateSumTest extends QueryBaseStaticTest {
+    private static String idA;
+    private static String idB;
+
+    @BeforeAll
+    static void setup() throws Exception {
+        run(tx -> {
+            Node courseA = tx.createNode(Label.label("Course"));
+            Node courseB = tx.createNode(Label.label("Course"));
+            Node bookA = tx.createNode(Label.label("Book"));
+            bookA.setProperty("price", 5.00);
+            Node bookB = tx.createNode(Label.label("Book"));
+            bookB.setProperty("price", 60.00);
+
+            RelationshipType edgeName = RelationshipType.withName("uses");
+            Relationship likesA = courseA.createRelationshipTo(bookA, edgeName);
+            likesA.setProperty("likes", 10);
+            likesA.setProperty("recommended", false);
+            Relationship likesB = courseA.createRelationshipTo(bookB, edgeName);
+            likesB.setProperty("likes", 20);
+            likesB.setProperty("recommended", true);
+            Relationship likesC = courseB.createRelationshipTo(bookA, edgeName);
+            likesC.setProperty("likes", 2);
+            likesC.setProperty("recommended", true);
+
+            tx.commit();
+            idA = String.valueOf(courseA.getId());
+            idB = String.valueOf(courseB.getId());
+        });
+    }
+
+    @Test
+    @DisplayName("[Table] Sum Edges by Node Group")
+    void sumEdgesByNodeGroup() throws Exception {
+        var query = "" +
+                "| Course | id* | uses              | likes*     |\n" +
+                "|--------+-----+-------------------+------------|\n" +
+                "| QUERY  |     | QUERY Course.Book | SUM Course |\n";
+        ResultGraph resultGraph = execute(query);
+        assertEquals(30, resultGraph.get(idA).property("likes"));
+        assertEquals(2, resultGraph.get(idB).property("likes"));
+    }
+
+    @Test
+    @DisplayName("[Table] Sum Nodes by Node Group")
+    void sumNodesByNodeGroup() throws Exception {
+        var query = "" +
+                "| Course | id* | uses              | Book  | price*     |\n" +
+                "|--------+-----+-------------------+-------+------------|\n" +
+                "| QUERY  |     | QUERY Course.Book | QUERY | SUM Course |\n";
+
+        ResultGraph resultGraph = execute(query);
+        assertEquals(65.00, resultGraph.get(idA).property("price"));
+        assertEquals(5.00, resultGraph.get(idB).property("price"));
+    }
+
+
+    @Test
+    @DisplayName("[Table] Sum Edges Filtered by Properties")
+    void sumFilteredEdges() throws Exception {
+        var query = "" +
+                "| Course | id* | uses              | likes*     | recommended* |\n" +
+                "|--------+-----+-------------------+------------+--------------|\n" +
+                "| QUERY  |     | QUERY Course.Book | SUM Course | true         |\n";
+        ResultGraph resultGraph = execute(query);
+        assertEquals(20, resultGraph.get(idA).property("likes"));
+        assertEquals(2, resultGraph.get(idB).property("likes"));
+    }
+
+    @Test
+    @DisplayName("[Table] Sum Nodes Filtered by Edges")
+    void sumNodesFilteredByEdges() throws Exception {
+        var query = "" +
+                "| Course | id* | price* | uses              | likes* |\n" +
+                "|--------+-----+--------|-------------------+--------|\n" +
+                "| QUERY  |     | SUM _  | QUERY Course.Book | > 10   |\n";
+        ResultGraph resultGraph = execute(query);
+        assertEquals(1, resultGraph.order());
+        assertEquals(60.0, resultGraph.get(idA).property("price"));
+    }
+}
